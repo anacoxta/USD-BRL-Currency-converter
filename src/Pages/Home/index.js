a17edc45b8279e3f30ad88413d1e0ae131de6f25
anacoxta <ana.costa@btwo.com.br>
2020-03-22 18:36:39 -0300

    Set up project

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container, Title} from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Title>USD &#128898; BRL</Title>
+
+    </Container>
+  );
+}
+
+export default Home;
